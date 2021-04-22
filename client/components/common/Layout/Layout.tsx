12cabf70d9a91e085f4dfa7f8b8b4c3685701858
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-04-22 13:10:44 +0200

    feat(Client): Add favicons

--- client/components/common/Layout/Layout.tsx
+++ client/components/common/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ReactChild } from 'react'
 import { Nav } from '../Nav/Nav'
 
@@ -8,6 +9,38 @@ interface LayoutProps {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <>
+      <Head>
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicons/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicons/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/favicons/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='/favicons/safari-pinned-tab.svg'
+          color='#5bbad5'
+        />
+        <link rel='shortcut icon' href='/favicons/favicon.ico' />
+        <meta name='msapplication-TileColor' content='#2b5797' />
+        <meta
+          name='msapplication-config'
+          content='/favicons/browserconfig.xml'
+        />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
       <Nav />
       {children}
     </>
