114a9d75e4b8f76f45ec666014e5a0a3984675f8
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-04-24 16:57:51 +0200

    feat(Client): Add legal notice

---
+++ client/pages/mentions-legales/index.tsx
@@ -0,0 +1,21 @@
+const MentionsLegales = () => {
+  return (
+    <main className='mentions-legales'>
+      <h1>Mentions Légales:</h1>
+      <section>
+        <h2>Mes coordonnées:</h2>
+        <p>Nom: HUSSON</p>
+        <p>Préno: Renaud</p>
+        <p>Numéro de téléphone: 06 78 91 02 75</p>
+      </section>
+      <section>
+        <h2>Hébergement:</h2>
+        <p>Nom: fastpipe.io</p>
+        <p>Numéro de téléphone: +49 8764 258 989 0</p>
+        <p>Adresse: Mitterfeld 47, 85419 Mauern, Allemagne</p>
+      </section>
+    </main>
+  )
+}
+
+export default MentionsLegales
