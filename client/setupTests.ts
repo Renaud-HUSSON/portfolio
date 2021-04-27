6906d9f837b5f8ff13c45e8ed7073a82ff4c36a8
Renaud-HUSSON <renaudhusson09@gmail.com>
2021-04-27 13:17:07 +0200

    test: Add window.matcheMedia mock to pass tests

--- client/setupTests.ts
+++ client/setupTests.ts
@@ -8,3 +8,17 @@ window.IntersectionObserver = jest
 
 window.HTMLMediaElement.prototype.play = async () => {}
 window.HTMLMediaElement.prototype.pause = async () => {}
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
